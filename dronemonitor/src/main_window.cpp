29f88104e1db85a3267bc1c66c61e9cd5472c39b
Kasper Høj Lorenzen <kasper.lorenzen@gmail.com>
2018-10-29 16:42:31 +0100

    Implemented set funxtions for dronemonitor

--- dronemonitor/src/main_window.cpp
+++ dronemonitor/src/main_window.cpp
@@ -11,6 +11,7 @@
 
 #include <QtGui>
 #include <QMessageBox>
+#include <QLocale>
 #include <iostream>
 #include "../include/dronemonitor/main_window.hpp"
 
@@ -50,24 +51,33 @@ void MainWindow::set_armed(bool isArmed){
 
 void MainWindow::set_status(int status){
     if(status == 1){
-        ui->label_status->setText(QString("In Air"));
+        ui->label_status->setText(QString("Running Mission"));
     }else if(status == 2){
-
+        ui->label_status->setText(QString("On Ground"));
     }else if(status == 3){
-
+        ui->label_status->setText(QString("On hold"));
     }
 }
 
 void MainWindow::set_battery(double SOC){
+    QString text;
+    QLocale obj;
+    text.push_back(obj.toString(SOC,'f',2));
+    text.push_back(QString("%"));
+    ui->label_battery->setText(text);
 
 }
 
 void MainWindow::set_time(string time){
-
+    ui->label_time->setText(time.c_str());
 }
 
 void MainWindow::set_uptime(double sec){
-
+    QString text;
+    QLocale obj;
+    text.push_back(obj.toString(sec,'f',2));
+    text.push_back(QString("s"));
+    ui->label_uptime->setText(text);
 }
 
 void MainWindow::set_RPY(vector<double> RPY){
@@ -75,30 +85,43 @@ void MainWindow::set_RPY(vector<double> RPY){
 }
 
 void MainWindow::set_heading(double heading){
-
+    QString text;
+    QLocale obj;
+    text.push_back(obj.toString(heading,'f',2));
+    //text.push_back(QString("s"));
+    ui->label_heading->setText(text);
 }
 
 void MainWindow::set_position(gcs::GPS pos){
 
 }
 void MainWindow::set_Height(double height){
-
+    QString text;
+    QLocale obj;
+    text.push_back(obj.toString(height,'f',1));
+    text.push_back(QString("m"));
+    ui->label_height->setText(text);
 }
 
 void MainWindow::set_velocity(double velocity){
-
+    QString text;
+    QLocale obj;
+    text.push_back(obj.toString(velocity,'f',2));
+    text.push_back(QString("m/s"));
+    ui->label_velocity->setText(text);
 }
 
 void MainWindow::set_autoPilot(string text){
-
+    ui->label_autoPilot->setText(text.c_str());
 }
 
 void MainWindow::set_subFlightmode(string text){
+    ui->label_filghtmode_sub->setText(text.c_str());
 
 }
 
 void MainWindow::set_mainFlightmode(string text){
-
+    ui->label_flightmode_main->setText(text.c_str());
 }
 
 void MainWindow::set_otherFlightmode(bool manual, bool simulation, bool stabilized, bool guided, bool autoM, bool test, bool custom){
@@ -106,11 +129,11 @@ void MainWindow::set_otherFlightmode(bool manual, bool simulation, bool stabiliz
 }
 
 void MainWindow::set_mavlinkState(string state){
-
+    ui->label_mavlinkState->setText(state.c_str());
 }
 
 void MainWindow::set_mavlinkType(string type){
-
+    ui->label_mavlinkType->setText(type.c_str());
 }
 
 /*****************************************************************************
