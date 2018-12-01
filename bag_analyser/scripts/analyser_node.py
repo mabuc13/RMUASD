878c0759bcc91d41603559b6e332c671bdc86109
Kristian Hansen <kmolha@gmail.com>
2018-12-01 13:30:49 +0100

    Create node for analysing ros bags

---
+++ bag_analyser/scripts/analyser_node.py
@@ -0,0 +1,146 @@
+#!/usr/bin/env python3
+import rospy
+import time
+import struct
+import rospkg
+import datetime
+import numpy as np
+import rmsd
+import copy
+import kalman
+import kalman_acc3
+from utm import utmconv
+from math import isnan
+
+from telemetry.msg import * # pylint: disable=W0614
+from mavlink_lora.msg import * # pylint: disable=W0614
+from geometry_msgs.msg import Point
+from precision_landing.msg import precland_sensor_data
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib import pyplot as plt
+plt.style.use('seaborn-whitegrid')
+
+# defines
+RECORDING_FLIGHT_MODE = "Altitude Control"
+TAG_ADDRESS = 13
+
+LANDING_TARGET_REF = 0
+
+# 3 floats amount to a length of 12 bytes
+LANDING_TARGET_SIZE = 12
+
+# parameters
+update_interval = 30
+
+class Analyser(object): 
+
+    def __init__(self):
+        self.rate = rospy.Rate(update_interval)
+
+        dt = 1/update_interval
+        self.state = np.zeros((4,1), float)
+        self.kalman = kalman.KalmanFilter(self.state, dt)
+        # self.state = np.zeros((6,1), float)
+        # self.kalman = kalman3.KalmanFilter(self.state, dt)
+        # self.state = np.zeros((9,1), float)
+        # self.kalman = kalman_acc3.KalmanFilter(self.state, dt)
+
+        self.local_position_ned = np.array([0,0,0])
+        self.sensor_data = np.array([0,0,0]) 
+        self.landing_coords = np.array([1.17, 0.75, 0])
+        self.new_pos_reading = False
+        self.new_sensor_reading = False
+        self.vx = 0
+        self.vy = 0
+        self.vz = 0
+ 
+        self.filtered_pos = np.empty((0,2), float)
+        self.landing_target = np.array([0,0,0])
+
+        self.data = np.empty((0,4), float)   
+        self.local_data = np.empty((0,3), float)
+        # self.filtered_data = np.empty((0,3), float)
+        self.filtered_data = np.empty((0,2), float)
+        self.rotation_matrix = np.array([[1,0],[0,1]])
+
+        self.fig = plt.gcf()
+        self.ax = self.fig.add_subplot(111, projection='3d')
+        self.fig.show()
+        self.fig.canvas.draw()
+
+
+        # rospy.Subscriber("/mavlink_pos", mavlink_lora_pos, self.on_drone_pos)
+        # rospy.Subscriber("/telemetry/mission_info", telemetry_mission_info, self.on_mission_info)
+        # rospy.Subscriber("/telemetry/heartbeat_status", telemetry_heartbeat_status, self.on_heartbeat_status)
+        # rospy.Subscriber("/telemetry/imu_data_ned", telemetry_imu_ned, self.on_imu_data)
+        
+        rospy.Subscriber("/telemetry/local_position_ned", telemetry_local_position_ned, self.on_local_pos)
+        rospy.Subscriber("/landing/sensor_data", precland_sensor_data, self.on_sensor_data)
+
+    def on_sensor_data(self, msg):
+        self.new_sensor_reading = True
+        self.sensor_data = np.array([msg.x, msg.y, msg.z])
+
+    def on_local_pos(self, msg):
+        self.new_pos_reading = True
+        self.local_position_ned = np.array([msg.x, msg.y, msg.z])
+        self.vx = msg.vx
+        self.vy = msg.vy
+        self.vz = msg.vz
+
+    def run(self):
+            
+        if self.new_pos_reading:
+            self.new_pos_reading = False
+            # compute something
+
+            self.ax.scatter(self.local_position_ned[1], self.local_position_ned[0], -self.local_position_ned[2], color='black', s=2) # plot something
+            
+            # print(self.local_position_ned)
+            # print(self.local_position_ned[0], self.local_position_ned[1])
+
+            # update canvas immediately
+            plt.xlim([-10, 10])
+            plt.ylim([-10, 10])
+            # plt.pause(0.01)  # I ain't needed!!!
+            self.fig.canvas.draw()
+            
+        if self.new_sensor_reading:
+            self.new_sensor_reading = False
+            # compute something
+
+            self.ax.scatter(self.sensor_data[1], self.sensor_data[0], self.sensor_data[2], color='red', s=2) # plot something
+
+            relative_target = self.landing_coords - self.sensor_data
+            added = relative_target + self.local_position_ned
+            # self.ax.scatter(relative_target[1], relative_target[0], -relative_target[2], color='blue', s=2) # plot something
+
+
+            
+            # print(self.sensor_data)
+            # print(self.sensor_data[0], self.sensor_data[1])
+
+            # update canvas immediately
+            plt.xlim([-10, 10])
+            plt.ylim([-10, 10])
+            #plt.pause(0.01)  # I ain't needed!!!
+            self.fig.canvas.draw()
+
+
+
+    def shutdownHandler(self):
+        # shutdown services
+        print("Shutting down")
+
+if __name__ == "__main__":
+    rospy.init_node('bag_analyser')
+    rospy.sleep(1)
+
+    al = Analyser()
+
+    rospy.on_shutdown(al.shutdownHandler)
+        
+    while not rospy.is_shutdown():
+        al.run()
+
+        al.rate.sleep()
---
+++ bag_analyser/scripts/kalman_acc3.py
@@ -0,0 +1,140 @@
+import numpy as np
+from numpy.linalg import inv, norm
+from numpy.random import randn
+from math import pi
+
+class KalmanFilter(object):
+
+    def __init__(self, initial_state, dt):
+        self.x      = initial_state
+        self.dt     = dt
+        # included acceleration
+        # x < - x + x_velocity
+        # y < - y + y_velocity
+        # x_velocity < - x_velocity + x_acceleration
+        # y_velocity < - y_velocity + y_acceleration
+        # x_acceleration < - x_acceleration
+        # y_acceleration < - y_acceleration
+        self.F      = np.array([[1, 0, 0, dt, 0, 0, dt*dt/2, 0, 0],
+                                [0, 1, 0, 0, dt, 0, 0, dt*dt/2, 0],
+                                [0, 0, 1, 0, 0, dt, 0, 0, dt*dt/2],
+                                [0, 0, 0, 1, 0, 0, dt, 0, 0],
+                                [0, 0, 0, 0, 1, 0, 0, dt, 0],
+                                [0, 0, 0, 0, 0, 1, 0, 0, dt],
+                                [0, 0, 0, 0, 0, 0, 1, 0, 0],
+                                [0, 0, 0, 0, 0, 0, 0, 1, 0],
+                                [0, 0, 0, 0, 0, 0, 0, 0, 1]])
+
+        # Measurements include position and accelleration in x and y
+        # TODO The accelleration should maybe be added as a control input instead of a measurement
+        self.H      = np.array([[1, 0, 0, 0, 0, 0, 0, 0, 0],
+                                [0, 1, 0, 0, 0, 0, 0, 0, 0],
+                                [0, 0, 1, 0, 0, 0, 0, 0, 0],
+                                [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                                [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                                [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                                [0, 0, 0, 0, 0, 0, 1, 0, 0],
+                                [0, 0, 0, 0, 0, 0, 0, 1, 0],
+                                [0, 0, 0, 0, 0, 0, 0, 0, 1]])
+
+        self.P_min  = np.eye(9)
+        self.P_plus = np.eye(9)
+
+        self.x_hat_min  = self.x
+        self.x_hat_plus = self.x
+
+        # Members for detecting self-destruct condition
+        # self.maxPredictions = maxPredictions
+        self.predictionCount = 0
+        self.selfDestruct = False
+
+        # Members for delaying drawing feature
+        self.correctionCount = 0
+        # self.startDelay = startDelay
+        # self.valid = False
+
+        # Error matrices from Agus lecture
+        measurementNoise    = 5
+        modelNoise          = 2
+        # TODO make sure matrix sizes make sense
+        self.Q      = np.eye(9)*modelNoise**2*dt
+        self.R      = np.eye(9)*measurementNoise**2/dt
+
+        # Error matrices from youtube
+        # measurementNoise = 0.5
+        # self.R      = np.eye(2)*measurementNoise**2/dt
+        # self.Q      = np.array([[pow(dt,4)/4,    0,              pow(dt,3)/2,    0],
+        #                         [0,              pow(dt,4)/4,    0,              pow(dt,3)/2],
+        #                         [pow(dt,3)/2,    0,              dt**2,          0],
+        #                         [0,              pow(dt,3)/2,    0,              dt**2]])*dt
+
+    def update(self, y=None):
+        # If there is no new measurement, then we can only make a prediction
+
+        x, P = self.predict()
+        x0 = self.x_hat_plus
+
+
+        # Members for detecting self-destruct condition
+        # self.maxPredictions = maxPredictions
+        self.predictionCount = 0
+        self.selfDestruct = False
+        if type(y) != type(None):
+            x, P = self.correct(y)
+            self.predictionCount = 0
+            self.correctionCount += 1
+
+        else:
+            # The prediction was the best we could do in this iteration, so x_hat_plus and P_plus
+            # are assigned to the result from the prediction
+            self.x_hat_plus = x
+            self.P_plus = P
+            self.predictionCount += 1
+
+        return x
+
+
+    def predict(self):
+        # For prediction we can only make a priori estimates
+        # We need to increase the uncertainty in P
+        self.P_min = self.F @ self.P_plus @ self.F.T + self.Q
+
+        # Then we need to make our state prediction based on our model
+        # a = randn()*
+        self.x_hat_min = self.F @ self.x_hat_plus# + self.G * a
+
+        # If this is all we do in this iteration then we need to update xhat_plus and P_plus as well
+        # with our prediction estimates from this function, because they are used at the beginning of each
+        # iteration
+
+        # self.x_hat_plus = self.x_hat_min
+        # self.P_plus     = self.P_min
+
+        # This can also be taken care of in the update function
+
+        return self.x_hat_min, self.P_min
+
+    def correct(self, y):
+        # Use long formula from book to calculate the kalman gain from P_min instead of P_plus
+        # This is necessary if P_plus is changed in the predict function
+        self.K = self.P_min@self.H.T@inv(self.H@self.P_min@self.H.T + self.R)
+
+        # P_plus has to be the P_plus from the previous iteration; it must not be calculated in the
+        # predict function
+        # self.K          = self.P_plus @ self.H.T @ inv(self.R)
+
+        self.x_hat_plus = self.x_hat_min + self.K @ (y - self.H @ self.x_hat_min)
+        self.P_plus     = (np.eye(9) - self.K @ self.H) @ self.P_min
+
+        return self.x_hat_plus, self.P_plus
+
+    def getState(self):
+        return self.x_hat_plus
+
+    def getPosition(self):
+        return np.array([self.x_hat_plus[0,0], self.x_hat_plus[1,0]])
+
+    def getAbsVelocity(self):
+        v = np.array([self.x_hat_plus[2, 0], self.x_hat_plus[3, 0]])
+        v = norm(v)
+        return v
