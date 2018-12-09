40947fb630fad408114f0f1e93e8e35383c06096
Kristian Hansen <kmolha@gmail.com>
2018-12-09 20:25:53 +0100

    Add visualization of kalman position

--- precision_landing/scripts/graph.py
+++ precision_landing/scripts/graph.py
@@ -35,8 +35,8 @@ class Graph(object):
         self.protection = threading.Lock()
 
 
-        # rospy.Subscriber("/landing/arduino_pos", precland_sensor_data, self.on_arduino_pos)
-        rospy.Subscriber("/landing/kalman_pos", precland_sensor_data, self.on_arduino_pos)
+        rospy.Subscriber("/landing/arduino_pos", precland_sensor_data, self.on_arduino_pos)
+        # rospy.Subscriber("/landing/kalman_pos", precland_sensor_data, self.on_arduino_pos)
         rospy.Subscriber("/telemetry/set_landing_target", telemetry_landing_target, self.on_set_landing_target)
 
         plt.axis([-5, 7, -5, 9])
